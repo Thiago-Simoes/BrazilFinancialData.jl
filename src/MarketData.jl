0f1cb345bad9434976d8668e60fdfcd0cc2d416d
Thiago Rodrigues <53097997+Thiago-Simoes@users.noreply.github.com>
2025-09-24 17:45:27 -0300

    Update and rename InternationalSources.jl to MarketData.jl

--- src/MarketData.jl
+++ src/MarketData.jl
@@ -1,4 +1,4 @@
-module InternationalSources
+module MarketData
 
 using Dates
 using DataFrames
@@ -14,20 +14,20 @@ import .MarketData
 
 export YahooOpt, yahoo
 
-function stocks(symbol::AbstractString; start_date::Date=Date(2000), end_date::Date=today(), ssl_verification::Bool = true)::TimeArray
+function stocks(symbol::AbstractString; start_date::Date=Date(2000), end_date::Date=today(), ssl_verification::Bool = true)::DataFrame
   opt = MarketData.YahooOpt(period1=DateTime(start_date), period2=DateTime(end_date))
   return MarketData.yahoo(symbol, opt, ssl_verification) |> DataFrame
 end
 
-stocks(symbol::Symbol; start_date::Date=Date(2000), end_date::Date=today(), ssl_verification::Bool = true)::TimeArray =
+stocks(symbol::Symbol; start_date::Date=Date(2000), end_date::Date=today(), ssl_verification::Bool = true)::DataFrame =
   stocks(string(symbol); start_date=start_date, end_date=end_date, ssl_verification=ssl_verification)
 
-function fred(series::AbstractString; start_date::Date=Date(2000), end_date::Date=today(), ssl_verification::Bool = true)::TimeArray
+function fred(series::AbstractString; start_date::Date=Date(2000), end_date::Date=today(), ssl_verification::Bool = true)::DataFrame
   data = MarketData.fred(series, opt, ssl_verification)
   filter!(x -> start_date <= Date(x.timestamp) <= end_date, data)
   return data |> DataFrame
 end
-fred(series::Symbol; start_date::Date=Date(2000), end_date::Date=today(), ssl_verification::Bool = true)::TimeArray =
+fred(series::Symbol; start_date::Date=Date(2000), end_date::Date=today(), ssl_verification::Bool = true)::DataFrame =
   return fred(string(series); start_date=start_date, end_date=end_date, ssl_verification=ssl_verification) |> DataFrame
 
 end
